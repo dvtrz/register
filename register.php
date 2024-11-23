50df5a55f13ff60f6369b131ba331b23e0f44067
devita <devitarezaa@gmail.com>
2024-11-23 14:57:54 +0700

    first init github

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+    if(isset($_POST['email'])) {
+        $nama = $_POST['nama'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $query = 
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTER</title>
+</head>
+<body>
+    <form action="" method="post">
+    
+    <table for="">MASUKKAN USERNAME</table>
+    <input type="text" name="nama" id="nama">
+
+    <label for="">MASUKKAN EMAIL</label>
+    <input type="email" name="email" id="email">
+
+    <label for="">MASUKKAN PASSWORD</label>
+    <input type="password" name="password" id="password">
+
+    </form>
+</body>
+</html>
